52e4e10f0ff8dc2ad95bc95ec3ac9ad6297d6636
AlexanderL <al.x.300000@gmail.com>
2021-01-04 17:52:08 -0500

    Update main.py
    Displays a lovely status

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import discord
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
+        await self.change_presence(activity = discord.Game(name = "Lovely day it is today!"))
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
 client = MyClient()
-client.run('my token goes here')
-
+client.run("token")
