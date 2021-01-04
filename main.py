2af2aa080dbfb3b846e4830061ae73532795b730
Lefan Hu <lefanhu1@gmail.com>
2021-01-04 17:13:51 -0500

    test code added to main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ("hi")
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run('my token goes here')
+
