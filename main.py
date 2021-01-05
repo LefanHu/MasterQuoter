5418c4b85a599a6b25e77661deb2199b65890340
Lefan Hu <lefanhu1@gmail.com>
2021-01-04 19:41:14 -0500

    added alex3000 response exception

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    await client.change_presence(activity=discord.Game(name="It's a lovely day!"))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.author.name == "Alex3000":
+        await message.channel.send('STFU ugly bastard, no one wants you here')
+        return
+
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('Your token here')
+client.run('Nzk1NzU2ODMyMTY0NDEzNTAw.X_OATQ.10gDrp9Zru7i32A78nH7G8T23UU')
