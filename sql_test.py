9da11055f06f77c142cc7c19c57ee826e86f1c01
Lefan Hu <lefanhu1@gmail.com>
2021-01-05 09:52:18 -0500

    started work on quote saving

--- sql_test.py
+++ sql_test.py
@@ -1,9 +1,11 @@
 import discord
+from discord.ext import commands
 import sqlite3
 from sqlite3 import Error
 
 client = discord.Client()
 sqlpath = "quotes.db"
+bot = commands.Bot(command_prefix='$')
 
 def create_connection(path):
     connection = None
@@ -15,31 +17,29 @@ def create_connection(path):
 
     return connection
 
-def save_quote(sqlpath, user, message, date)
+@bot.command()
+async def save(ctx, user):
     conn = create_connection(sqlpath)
     
     #creating cursor for selection in sql
     c = conn.cursor()
     #checking if table exists
     sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='{quotes}'"
-    if (c.execute(sql) == false):
-        print("The table does not exist, attempting to create one...")
+    if (c.execute(sql) == False):
+        ctx.send("The table does not exist, attempting to create one...")
         #create table here
         try:
             #attempting to create table in sql database
             c.execute('''CREATE TABLE quotes (userid, date, message)''')
         except Error as e:
-            print(f"The error '{e}' occurred")    
+            ctx.send(f"The error '{e}' occurred")    
     else:
-        print("The table exists already. Saving quote.")
+        ctx.send("The table exists already. Saving quote.")
         #saving the quote
         try:
             c.execute("INSERT INTO quotes VALUES (?, ?, ?)", user, message, date)
         except Error as e:
-            print(f"The error '{e}' occurred")
-            
-
-
+            ctx.send(f"The error '{e}' occurred")
 
 @client.event
 async def on_ready():
@@ -56,13 +56,23 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+        return
 
     if message.content.startswith('$quote'):
         #splitting the message by spaces
-        message.split()
+        message.content.split()
+        return
+    
+    if message.content.startswith('$ping '):
+        #splitting th emessage by spaces
+        message.content.split()
+        return
+    
+    if message.content.startswith('$return '):
+        #splitting message
+        string = message.content.split()
+        print('USERID = {0}'.format(string[1]))
         
-
-
 
 #running the bot
 client.run('Nzk1NzU2ODMyMTY0NDEzNTAw.X_OATQ.__1rDuwUZoDWcdKm0wwZknUOixY')
