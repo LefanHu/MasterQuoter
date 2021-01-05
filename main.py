9011362e3481873f0a3ba9c48e3a2c1520f47836
Lefan Hu <lefanhu1@gmail.com>
2021-01-04 19:06:26 -0500

    fixed main.py

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 import discord
-import logging
-import sys
 
+client = discord.Client()
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-        await self.change_presence(activity = discord.Game(name = "Lovely day it is today!"))
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-    #outputting logs to discord.log file instead of to stdout
-    logger = logging.getLogger('discord')
-    logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-    logger.addHandler(handler)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
-    #testing the use of the discord logger
-    sys.exit('Test error message hopefully to be seen in file "discord.log".')
-
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-
-client = MyClient()
-client.run("Your token here")
+client.run('your token here')
