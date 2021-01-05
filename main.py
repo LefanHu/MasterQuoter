cb2d2e7d274c00cbb5dcfa1cd87515d0732a052a
AlexanderL <al.x.300000@gmail.com>
2021-01-04 21:10:00 -0500

    Some troll code
    I'm immature, but this is just a joke

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    elif message.author.name == 'Alex3000' and message.content.startswith("hello"):
+        await message.channel.send('Welcome Master Baiter')
+        return
+
+    elif message.author.name == 'Cuddles' and message.content.startswith("hello"):
+        await message.channel.send('Welcome Master Baiter')
+        return
+
+    elif message.author.name == 'AustZW':
+        await message.channel.send('Austin likes big black balls')
+        return
+
     elif message.content.startswith('$goodbye'):
         await message.channel.send('Bye bye!')
         return
@@ -23,5 +35,10 @@ async def on_message(message):
         n = random.randint(1,6)
         await message.channel.send('You rolled a {0}!'.format(n))
 
+    elif message.content.startswith('$quote lefan'):
+        lefquotes = ["my balls are huge", "i have a three milimeter defeater", "i will punish you with my one inch punisher", "alex pp is massive"]
+        lefquotechoice = random.choice(lefquotes)
+        await message.channel.send(lefquotechoice)
 
-client.run('enter token here')
+
+client.run('token')
