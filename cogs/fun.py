8b5868026204f1fc6a241bb7aa6fc09a6dfc63e9
Lefan Hu <lefanhu1@gmail.com>
2021-02-13 17:49:35 -0500

    added fun.py cog

---
+++ cogs/fun.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import os
+
+
+class events(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def loaded_questions(self, ctx):
+        ctx.send("games ")
+
+
+def setup(bot):
+    bot.add_cog(events(bot))
+    print(f"Cog '{os.path.basename(__file__)}' has been loaded")
