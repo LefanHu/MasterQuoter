f3b8c45ab67c72a11c335b2a5d2d8b83aa9ea476
Lefan Hu <lefanhu1@gmail.com>
2021-02-13 12:29:28 -0500

    first commit

---
+++ cogs/Help_Commands.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+
+class Example(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+def setup(bot):
+    bot.add_cog(Example(bot))
+    print(f"Cog 'Example' has been loaded")
---
+++ cogs/events.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+
+class events(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+
+def setup(client):
+    client.add_cog(events(client))
+    print(f"Cog 'Events' has been loaded")
