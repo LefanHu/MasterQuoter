6f093385f731895255cdc677e87747b1a5dad2c8
Lefan Hu <lefanhu1@gmail.com>
2021-02-13 20:37:45 -0500

    now saves channel name instead of channel id

--- cogs/Save.py
+++ cogs/Save.py
@@ -13,6 +13,13 @@ class Save(commands.Cog):
 
         self.quote_buffer = []
 
+    # getting a sample dataset
+    @commands.command(aliases=["slh"])
+    async def save_last_hundred(self, ctx):
+        messages = await ctx.channel.history(limit=100).flatten()
+        for message in messages:
+            await self.quote(ctx=ctx, user=message.author, msg=message.content)
+
     # Saving attachments associated with a message
     async def save_attachments(self, message):
         attachments = []
@@ -34,7 +41,7 @@ class Save(commands.Cog):
             "display_name": user.display_name,
             "time": "{}".format(ctx.message.created_at.strftime("%m/%d/%Y, %H:%M:%S")),
             "attachments": await self.save_attachments(ctx.message),
-            "channel": ctx.message.channel,
+            "channel": ctx.message.channel.name,
         }
         server_id = str(ctx.message.guild.id)
         mem_id = str(user.id)
@@ -44,7 +51,6 @@ class Save(commands.Cog):
 
         # adds the qutoe to the buffer
         self.quote_buffer.append(array)
-        await ctx.send("quote has been appended")
 
     # Clears buffer
     @tasks.loop(seconds=2.0)
@@ -76,7 +82,7 @@ class Save(commands.Cog):
                     quote = quote[2]
 
                     if server_id not in file:  # server has not used bot
-                        print("server id does not exist")
+                        # print("server id does not exist")
                         qt = {}
                         qt["quotes"] = [quote]
 
@@ -89,7 +95,7 @@ class Save(commands.Cog):
                         file.update(server)
 
                     elif mem_id not in file[server_id]:  # member has not been quoted
-                        print("server id exists, userid does not")
+                        # print("server id exists, userid does not")
 
                         qt = {}
                         qt["quotes"] = [quote]
@@ -99,7 +105,7 @@ class Save(commands.Cog):
 
                         file[server_id].update(member)
                     else:  # adding another quote to user
-                        print("server id & member id exists")
+                        # print("server id & member id exists")
                         file[server_id][mem_id]["quotes"].append(quote)
             File(self.client).write_json(file, save_location)
             self.quote_buffer.clear()
