d0452a27a254e587fe0a500c52e1f4fbe99682c6
Lefan Hu <lefanhu1@gmail.com>
2021-02-15 18:06:29 -0500

    now waits till bot is ready

--- cogs/bot_stats.py
+++ cogs/bot_stats.py
@@ -5,15 +5,6 @@ from cogs.utils import utils
 
 
 class _bot_stats(commands.Cog):
-    tracked_statuses = {
-        "servers_count": 0,
-        "servers_joined": 0,
-        "member_count": 0,
-        "quotes_saved": 0,
-        "commands_processed": 0,
-        "err_report_count": 0,
-    }
-
     def __init__(self, bot):
         self.bot = bot
         self.file = File(self.bot)
@@ -22,41 +13,55 @@ class _bot_stats(commands.Cog):
         self.stat_file = self.file.get_env("STATS")
         self.stats = self.file.read_json(self.stat_file)
 
-        for stat in _bot_stats.tracked_statuses:
+        self.tracked_statuses = {
+            "servers_count": 0,
+            "servers_joined": 0,
+            "member_count": 0,
+            "quotes_saved": 0,
+            "commands_processed": 0,
+            "err_report_count": 0,
+        }
+
+        for stat in self.tracked_statuses:
             try:
                 stat_value = 0 if self.stats[stat] is None else self.stats[stat]
+                print(stat_value)
             except KeyError:
                 stat_value = 0
-            _bot_stats.tracked_statuses[stat] = stat_value
+            self.tracked_statuses[stat] = stat_value
 
         self.update_stats.start()
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        _bot_stats.tracked_statuses["server_count"] += 1
-        _bot_stats.tracked_statuses["servers_joined"] += 1
+        self.tracked_statuses["server_count"] += 1
+        self.tracked_statuses["servers_joined"] += 1
 
     @commands.Cog.listener()
     async def on_server_removed(self, guild):
-        _bot_stats.tracked_statuses["server_count"] -= 1
+        self.tracked_statuses["server_count"] -= 1
 
     @commands.Cog.listener()
     async def on_command(self, ctx):
-        _bot_stats.tracked_statuses["commands_processed"] += 1
+        self.tracked_statuses["commands_processed"] += 1
 
         if ctx.command == "compose_report":
-            _bot_stats.tracked_statuses["err_report_count"] += 1
+            self.tracked_statuses["err_report_count"] += 1
 
     @commands.Cog.listener()
     async def on_command_completion(self, ctx):
         if ctx.command == "quote":
-            _bot_stats.tracked_statuses["quotes_saved"] += 1
+            self.tracked_statuses["quotes_saved"] += 1
 
     @tasks.loop(minutes=5.0)
     async def update_stats(self):
-        _bot_stats.tracked_statuses["member_count"] = await self.utils.member_count()
-        _bot_stats.tracked_statuses["servers_count"] = len(self.bot.guilds)
-        self.file.write_json(_bot_stats.tracked_statuses, self.stat_file)
+        self.tracked_statuses["member_count"] = await self.utils.member_count()
+        self.tracked_statuses["servers_count"] = len(self.bot.guilds)
+        self.file.write_json(self.tracked_statuses, self.stat_file)
+
+    @update_stats.before_loop
+    async def before_update_stats(self):
+        await self.bot.wait_until_ready()
 
 
 def setup(bot):
