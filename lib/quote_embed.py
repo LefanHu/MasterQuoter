b67ec9a3d098fbea7ae742a97cec186972eb7ff1
Lefan Hu <lefanhu1@gmail.com>
2021-02-18 15:23:32 -0500

    minor changes to variablenames

--- lib/quote_embed.py
+++ lib/quote_embed.py
@@ -1,13 +1,15 @@
+from random import choice
 from discord import Embed, Colour
 from datetime import datetime as dt
 
 
 class embed:
-    def format(self, quote, *, attachment=None, hide_user=False):
+    def format(self, quote, *, image_num=None, hide_user=False):
         name = quote["display_name"] if not hide_user else "Unknown"
         avatar = quote["avatar_url"] if not hide_user else None
         msg = quote["msg"].join("\n") if type(quote["msg"]) == list else quote["msg"]
-        attachment = 0 if attachment == None else attachment
+        image_num = 0 if image_num == None else image_num
+        descriptor = ["wisdom", "quote", "statement"]
 
         embed = Embed(
             title=f"{name}:",
@@ -18,7 +20,7 @@ class embed:
 
         if hide_user:
             embed.set_author(name=name)
-            embed.set_footer(text=f"{name}'s wisdom")
+            embed.set_footer(text=f"{name}'s {choice(descriptor)}")
         else:
             embed.set_author(
                 name=name,
@@ -30,11 +32,10 @@ class embed:
                 icon_url=avatar,
             )
 
-        if not quote["attachments"]:
+        if not quote["image_attachments"]:
             pass
         else:
-            # this will change if the original attachment message has been deleted
-            embed.set_image(url=quote["attachments"][attachment]["url"])
+            embed.set_image(url=quote["image_attachments"][image_num]["url"])
 
         # dealing with attachments
 
