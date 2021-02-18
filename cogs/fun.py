c7e472e3bdc05e9ebc72f686b73abd4648181013
Lefan Hu <lefanhu1@gmail.com>
2021-02-18 15:31:08 -0500

    specifies # of guesses

--- cogs/fun.py
+++ cogs/fun.py
@@ -20,7 +20,7 @@ class events(commands.Cog):
         emb = embed().format(quote, hide_user=True)
 
         await ctx.send(
-            f"Guess who said this quote! Make at least 1 guess every 30 seconds.",
+            f"Guess who said this quote! (5 guesses) Make at least 1 guess every 30 seconds.",
             embed=emb,
         )
 
