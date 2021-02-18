90e6304c6af4d27f41557d0e116812ab5627f8fe
Lefan Hu <lefanhu1@gmail.com>
2021-02-18 11:09:41 -0500

    completed basic guessing game

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+from asyncio import TimeoutError
 
 from random import choice
 from lib.file_utils import File
@@ -13,12 +14,46 @@ class events(commands.Cog):
 
     @commands.command(aliases=["lq"])
     async def loaded_questions(self, ctx):
+        attempts = 5
+
         quote = choice(File().from_server(ctx.guild.id))
-        quote = embed().format(quote)
+        emb = embed().format(quote, hide_user=True)
 
-        # quote = embed().format(quote)
+        await ctx.send(
+            f"Guess who said this quote! Make at least 1 guess every 30 seconds.",
+            embed=emb,
+        )
 
-        await ctx.send(f"Guess who said this quote!", embed=quote)
+        def is_correct(msg):
+            return msg.author == ctx.message.author
+
+        while attempts > 0:
+            try:
+                guess = await self.bot.wait_for(
+                    "message", check=is_correct, timeout=30.0
+                )
+
+                if not guess.mentions:
+                    pass
+                elif len(guess.mentions) > 1:
+                    attempts -= 1
+                    await ctx.send(
+                        f"You mentioned {len(guess.mentions)} users in your guess. You have {attempts} guesses left."
+                    )
+                else:
+                    if guess.mentions[0].id == quote["user_id"]:
+                        await ctx.send(
+                            "You've guessed correctly!", embed=embed().format(quote)
+                        )
+                        break
+                    else:
+                        attempts -= 1
+                        await ctx.send(f"Incorrect. {attempts} guesses left")
+            except TimeoutError:
+                attempts -= 1
+                await ctx.send(
+                    f"You took too long to guess. You now have {attempts} guesses."
+                )
 
 
 def setup(bot):
--- lib/quote_embed.py
+++ lib/quote_embed.py
@@ -3,9 +3,9 @@ from datetime import datetime as dt
 
 
 class embed:
-    def format(self, quote, *, attachment=None):
-        name = quote["display_name"]
-        avatar = quote["avatar_url"]
+    def format(self, quote, *, attachment=None, hide_user=False):
+        name = quote["display_name"] if not hide_user else "Unknown"
+        avatar = quote["avatar_url"] if not hide_user else None
         msg = quote["msg"].join("\n") if type(quote["msg"]) == list else quote["msg"]
         attachment = 0 if attachment == None else attachment
 
@@ -16,15 +16,19 @@ class embed:
             timestamp=dt.fromtimestamp(quote["time_stamp"]),
         )
 
-        embed.set_author(
-            name=name,
-            icon_url=avatar,
-        )
+        if hide_user:
+            embed.set_author(name=name)
+            embed.set_footer(text=f"{name}'s wisdom")
+        else:
+            embed.set_author(
+                name=name,
+                icon_url=avatar,
+            )
 
-        embed.set_footer(
-            text=f"{name}'s wisdom",
-            icon_url=avatar,
-        )
+            embed.set_footer(
+                text=f"{name}'s wisdom",
+                icon_url=avatar,
+            )
 
         if not quote["attachments"]:
             pass
