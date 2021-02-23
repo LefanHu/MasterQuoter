0e772ab856a98416a829319f940b86ffb85cef5a
Lefan Hu <lefanhu1@gmail.com>
2021-02-22 19:15:48 -0500

    replaced os.path.basename with file()

--- cogs/report.py
+++ cogs/report.py
@@ -1,24 +1,21 @@
-import os
 import discord
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
 from lib.file_utils import File
 from datetime import datetime as dt
 
+
 class events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
-    async def dm(self, user: discord.Member,embed):
+    async def dm(self, user: discord.Member, embed):
         if user.dm_channel == None:
             channel = await user.create_dm()
         else:
             channel = user.dm_channel
 
-        await channel.send(embed = embed)
-
-
+        await channel.send(embed=embed)
 
     @commands.command(brief="Reports an issue to developers")
     @commands.dm_only()
@@ -39,12 +36,12 @@ class events(commands.Cog):
 
         msg = ctx.message.clean_content
         embed = discord.Embed(timestamp=dt.utcnow(), colour=0x00FFFF)
-        embed.add_field(name="FROM:", value =f"From: {ctx.message.author}")
-        embed.add_field(name="REPORT",  value=f"Msg: {msg}", inline=False )
+        embed.add_field(name="FROM:", value=f"From: {ctx.message.author}")
+        embed.add_field(name="REPORT", value=f"Msg: {msg}", inline=False)
 
         for attachment in ctx.message.attachments:
             img = attachment.url
-            embed.set_image(url = img)
+            embed.set_image(url=img)
         # Sends the error to direct messages
         developers = File().getenv("DEVELOPERS").strip("][").split(", ")
 
@@ -57,4 +54,4 @@ class events(commands.Cog):
 
 def setup(bot):
     bot.add_cog(events(bot))
-    print(f"Cog '{os.path.basename(__file__)}' has been loaded")
+    print(f"Cog '{File().file_name(__file__)}' has been loaded")
