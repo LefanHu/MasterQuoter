4f64a22f95538c2a9883395cb27a452e7e709705
Lefan Hu <lefanhu1@gmail.com>
2021-02-24 10:21:34 -0500

    testing of per server prefixes

---
+++ cogs/settings.py
@@ -0,0 +1,24 @@
+from discord.ext import commands
+from lib.db import db
+import os
+
+
+class Settings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(brief="Changes the prefix of this bot for your server")
+    async def prefix(self, ctx, *, prefix):
+        if len(prefix) > 5:
+            await ctx.send("prefixes cannot be longer than 5 characters")
+        else:
+            db.servers.find({"_id": ctx.guild.id}, {"$set": {"prefix": prefix}})
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if not self.bot.ready:
+            self.bot.cogs_ready.ready_up(os.path.basename(__file__))
+
+
+def setup(bot):
+    bot.add_cog(Settings(bot))
