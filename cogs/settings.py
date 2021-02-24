6bab0e0dc593394020b239ea6fee0628685c7ef0
Lefan Hu <lefanhu1@gmail.com>
2021-02-24 16:14:25 -0500

    added basic server stat functionality

--- cogs/settings.py
+++ cogs/settings.py
@@ -18,22 +18,34 @@ class Settings(commands.Cog):
             db.servers.update_one({"_id": ctx.guild.id}, {"$set": {"prefix": prefix}})
             await ctx.send("Prefix set.")
 
-    @commands.command(brief="Shows settings of the server & stats")
-    async def settings(self, ctx):
+    @commands.command(
+        aliases=["settings"], brief="Shows settings of the server & stats"
+    )
+    async def stats(self, ctx):
         settings = db.servers.find_one({"_id": ctx.guild.id})
+        guild = ctx.guild
 
+        # Title, description, inline(boolean)
         available_settings = [
-            ("Member Count", ctx.guild.member_count, True),
+            ("Members Quoted", len(settings["quoted_member_ids"]), True),
             ("Quotes Saved", settings["quotes_saved"], True),
             ("Commands Invoked", settings["commands_invoked"], True),
+            ("Server ID", guild.id, False),
+            ("Bot prefix", settings["prefix"], True),
         ]
 
         embed = Embed(
-            title="📉MasterQuoter Stats📉",
-            colour=0x00FFFF,
-            thumbnail=self.bot.user.avatar_url,
+            title=f"{guild.name}",
+            description=f"{'No description' if guild.description == None else guild.description}",
+            colour=ctx.message.author.colour,
+            thumbnail=guild.icon_url,
             timestamp=datetime.utcnow(),
         )
+
+        for name, value, inline in available_settings:
+            embed.add_field(name=name, value=f"```{value}```", inline=inline)
+
+        await ctx.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_ready(self):
