756d0670a0f23d3fd4087e94116d3407565d567f
Lefan Hu <lefanhu1@gmail.com>
2021-02-25 10:11:11 -0500

    organized some code into functions & caught some bugs

--- cogs/read.py
+++ cogs/read.py
@@ -55,49 +55,14 @@ class Read(commands.Cog):
                 ctx, quote["quotes"][0], message=f"Quote_ID: {message_id}"
             )
 
-    @commands.command(
-        name="qlist", aliases=["qfrom"], brief="lists all quotes from user"
-    )
-    async def quotes_from(self, ctx, user: Optional[discord.Member]):
+    @commands.command(aliases=["qfrom"], brief="lists all quotes from user")
+    async def qlist(self, ctx, user: Optional[discord.Member]):
         """Lists all quotes from a specified user"""
-
-        quotes = []
-
         if not user:
-
-            # getting quoted members from server in database
-            member_ids = db.servers.find_one(
-                {"_id": ctx.message.guild.id}, {"_id": 0, "quoted_member_ids": 1}
-            )["quoted_member_ids"]
-
-            # getting quotes from listed users in database
-            cursor = db.users.find(
-                {"_id": {"$in": member_ids}},
-                {
-                    "_id": 0,
-                    "quotes": 1,
-                },
-            )
-
-            quote_sections = list(cursor)
-            for quote_section in quote_sections:
-                for quote in quote_section["quotes"]:
-                    if quote["server_id"] == ctx.message.guild.id:
-                        quotes.append(quote)
-        else:
-            user_quotes = db.users.find_one({"_id": user.id}, {"_id": 0, "quotes": 1})[
-                "quotes"
-            ]
-            for quote in user_quotes:
-                if quote["server_id"] == ctx.message.guild.id:
-                    quotes.append(quote)
-
-        if not quotes:
-            await ctx.send(f"There are no quotes.")
-            return
-
-        # sends differently depending on if a user was specified
-        if not user:
+            quotes = await self.from_server(ctx.guild.id)
+            if not quotes:
+                await ctx.send("There are no quotes on this server")
+                return
             pages = MenuPages(
                 source=QuoteMenu(ctx, quotes, ctx.guild.name, ctx.guild.icon_url),
                 clear_reactions_after=True,
@@ -105,6 +70,10 @@ class Read(commands.Cog):
             )
             await pages.start(ctx)
         else:
+            quotes = await self.from_user(ctx.guild.id, user.id)
+            if not quotes:
+                await ctx.send("There are no quotes from this user")
+                return
             pages = MenuPages(
                 source=QuoteMenu(ctx, quotes, user.name, user.avatar_url),
                 clear_reactions_after=True,
@@ -118,40 +87,69 @@ class Read(commands.Cog):
                 message, embed=Emb().format_quote(quote, hide_user=hide_user)
             )
         else:  # deal with quotes with multiple attachments here
-            quote = ImageMenu(Emb.format_quote(quote), quote["image_attachments"])
+            quote = ImageMenu(Emb().format_quote(quote), quote["image_attachments"])
             await quote.start(ctx)
 
-    @commands.command(name="rand", aliases=["random"])
-    async def rand_from_server(self, ctx, user: Optional[discord.Member]):
-        """This command will fetch a random quote from your server and send it if no user is specified.\nIf a user is specified, this will fetch a random quote from that user."""
+    @commands.command(aliases=["random"], brief="Gives a random saved quote")
+    async def rand(self, ctx, user: Optional[discord.Member]):
+        """
+        This command will fetch a random quote from your server and send it if no user is specified.
+        If a user is specified, this will fetch a random quote from that user.
+        """
 
         if not user:
-            rand_user_id = random.choice(
-                db.servers.find_one(
-                    {"_id": ctx.message.guild.id},
-                    {"_id": 0, "quoted_member_ids": 1},
-                )["quoted_member_ids"]
-            )
-            quote = random.choice(
-                db.users.find_one({"_id": rand_user_id}, {"_id": 0, "quotes": 1})[
-                    "quotes"
-                ]
-            )
-
-            if not quote:
+            quotes = await self.from_server(ctx.guild.id)
+            if not quotes:
                 await ctx.send("There are no quotes in this server. ")
             else:
-                await self.send_quote(ctx, quote)
+                await self.send_quote(ctx, random.choice(quotes))
         else:  # if a user is specified
-            quote = random.choice(
-                db.users.find_one({"user_id": user.id}, {"_id": 0, "quotes": 1})[
-                    "quotes"
-                ]
-            )
-            if not quote:
+            quotes = await self.from_user(ctx.guild.id, user.id)
+            if not quotes:
                 await ctx.send("There are no quotes from this user on this server.")
             else:
+                quote = random.choice(quotes["quotes"])
                 await self.send_quote(ctx, quote)
+
+    async def from_server(self, guild_id):
+        try:
+            member_ids = db.servers.find_one(
+                {"_id": guild_id}, {"_id": 0, "quoted_member_ids": 1}
+            )["quoted_member_ids"]
+
+            # getting quotes from listed users in database
+            cursor = db.users.find(
+                {"_id": {"$in": member_ids}},
+                {
+                    "_id": 0,
+                    "quotes": 1,
+                },
+            )
+
+            # filters all quotes not from this server
+            quotes = []
+            quote_sections = list(cursor)
+            for quote_section in quote_sections:
+                for quote in quote_section["quotes"]:
+                    if quote["server_id"] == guild_id:
+                        quotes.append(quote)
+
+            return quotes
+        except TypeError:  # likely no server found
+            return []
+
+    async def from_user(self, guild_id, user_id):
+        try:
+            all_quotes = db.users.find_one({"_id": user_id}, {"_id": 0, "quotes": 1})
+
+            quotes = []
+            for quote in all_quotes["quotes"]:
+                if quote["server_id"] == guild_id:
+                    quotes.append(quote)
+
+            return quotes
+        except TypeError:  # likely no server found
+            return []
 
     @commands.Cog.listener()
     async def on_ready(self):
