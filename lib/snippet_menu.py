9e6ca291d6d62db03fc756ea1b9cbb491a28f468
Lefan Hu <lefanhu1@gmail.com>
2021-02-28 22:40:54 -0500

    reworked format_page

--- lib/snippet_menu.py
+++ lib/snippet_menu.py
@@ -2,12 +2,15 @@ from discord.ext.menus import ListPageSource
 from discord import Embed
 from datetime import datetime as dt
 
+from lib.utils import Utils
+
 
 class SnippetMenu(ListPageSource):
     def __init__(self, ctx, data, name=None, icon_url=None):
         self.ctx = ctx
         self.name = name
         self.icon_url = icon_url
+        self.utils = Utils()
         super().__init__(data, per_page=3)
 
     async def write_page(self, menu, fields=[]):
@@ -38,17 +41,8 @@ class SnippetMenu(ListPageSource):
     async def format_page(self, menu, entries):
         fields = []
         for snip in entries:
-            description = ""
-
-            # trimming down the message to first 1000 characters
-            for section in snip["sections"]:
-                description += f"\n-{section['author_name']}-{section['message']}"
-
-                if section["images"] != 0:
-                    description += f"\n+{section['images']} images"
-
-            description = description[0:200]
-            description.strip("`")
+            descriptions = self.utils.split_snip(snip, max_new_lines=8, max_chars=1000)
+            description = descriptions[0]
 
             fields.append(
                 (
