4996437e6334f03cd181655df0fa2c895ff1a139
AlexanderL <al.x.300000@gmail.com>
2021-04-16 20:44:29 -0400

    hid the database link

--- website/app.js
+++ website/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Stat = require('./models/stat');
+const dotenv = require('dotenv').config({path:'../.env'});
 
 //express app
 const app = express();
 
 //connect to mongodb
-const dbURI = '';
+const dbURI = process.env.DATABASE_URL;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true } )
     .then((result)=> app.listen(3000))
     .catch((err) => console.log(err));
