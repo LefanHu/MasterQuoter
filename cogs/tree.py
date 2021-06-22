39897002cb75f32988063090bb1daeac2b479420
AlexanderL <al.x.300000@gmail.com>
2021-06-22 12:46:51 -0400

    new tree cog

---
+++ cogs/tree.py
@@ -0,0 +1,53 @@
+from discord.ext import commands
+import os
+import requests
+from bs4 import BeautifulSoup
+from anytree import Node, RenderTree
+
+class Tree(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["pr"], hidden=True)
+    @commands.cooldown(1, 5, commands.BucketType.guild)
+    async def parent(self, ctx, item):
+
+        URL = 'https://calamitymod.fandom.com/wiki/' + str(item)
+        page = requests.get(URL)
+        lst = []
+
+        soup = BeautifulSoup(page.content, 'html.parser')
+        crafts = soup.find('td', class_='ingredients')
+        parent = soup.find('td', class_='result')
+
+        if(parent == None):
+            await ctx.send("This item does not craft anything. ")
+            return
+
+        ingredients = crafts.find_all('span', class_ = "i")
+        for ingredient in ingredients:
+            lst.append(ingredient)
+
+        top = Node(parent.text, parent = None)
+        for i in range(len(lst)):
+            Node(lst[i].text, parent=top)
+
+
+        out = "\n"
+        for pre, fill, node in RenderTree(top):
+            out += "%s%s\n" % (pre, node.name)
+            
+        await ctx.send("```"+out+"```")   
+
+    
+
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if not self.bot.ready:
+            self.bot.cogs_ready.ready_up(os.path.basename(__file__)[:-3])
+
+
+def setup(bot):
+    bot.add_cog(Tree(bot))
