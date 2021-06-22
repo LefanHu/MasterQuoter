dca2a522d54dd2da6d527ee13ccfadfc5296c92d
Lefan Hu <lefanhu1@gmail.com>
2021-06-22 13:38:34 -0400

    craft tree

--- cogs/tree.py
+++ cogs/tree.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 from anytree import Node, RenderTree
 
+
 class Tree(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -12,36 +13,31 @@ class Tree(commands.Cog):
     @commands.cooldown(1, 5, commands.BucketType.guild)
     async def parent(self, ctx, item):
 
-        URL = 'https://calamitymod.fandom.com/wiki/' + str(item)
+        URL = "https://calamitymod.fandom.com/wiki/" + str(item)
         page = requests.get(URL)
         lst = []
 
-        soup = BeautifulSoup(page.content, 'html.parser')
-        crafts = soup.find('td', class_='ingredients')
-        parent = soup.find('td', class_='result')
+        soup = BeautifulSoup(page.content, "html.parser")
+        crafts = soup.find("td", class_="ingredients")
+        parent = soup.find("td", class_="result")
 
-        if(parent == None):
+        if parent == None:
             await ctx.send("This item does not craft anything. ")
             return
 
-        ingredients = crafts.find_all('span', class_ = "i")
+        ingredients = crafts.find_all("span", class_="i")
         for ingredient in ingredients:
             lst.append(ingredient)
 
-        top = Node(parent.text, parent = None)
+        top = Node(parent.text, parent=None)
         for i in range(len(lst)):
             Node(lst[i].text, parent=top)
-
 
         out = "\n"
         for pre, fill, node in RenderTree(top):
             out += "%s%s\n" % (pre, node.name)
-            
-        await ctx.send("```"+out+"```")   
 
-    
-
-
+        await ctx.send("```" + out + "```")
 
     @commands.Cog.listener()
     async def on_ready(self):
